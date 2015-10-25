3341abba0d665b3b643bf25a079e9bf75b109b0b
edek437 <adrianwykrzykowski@gmail.com>
2015-10-25 13:19:03 +0100

    test pages, schedule page only for staff

--- Zastosowanie_informatyki_w_gospodarce/lotnisko/urls.py
+++ Zastosowanie_informatyki_w_gospodarce/lotnisko/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     # TODO: add test url
     url(r'^$', views.lotnisko_test, name='index'),
+    url(r'^mySchedule', views.schedule_test, name='schedule'),
+    url(r'^reservation', views.reservation_test, name='reservation'),
 ]
 
--- Zastosowanie_informatyki_w_gospodarce/lotnisko/views.py
+++ Zastosowanie_informatyki_w_gospodarce/lotnisko/views.py
@@ -5,4 +5,15 @@ from django.shortcuts import render
 
 def lotnisko_test(request):
     print "Django say something"
-    return render(request, "lotnisko/test.html", {})
+    return render(request, "lotnisko/lotnisko_main_page.html", {})
+
+
+def schedule_test(request):
+    name = 'Adrian'  # Test passing variables to html
+    print "Schedule testing"
+    return render(request, "lotnisko/schedule.html", {'name': name, 'user': request.user})  # TODO: w klamrach powiniem podac uzytkownika ktory chce zobaczyc schdule
+
+
+def reservation_test(request):
+    print "Reservation testing"
+    return render(request, "lotnisko/reservation_page.html", {})
